a0093d453d51622e2b32d52b45249f76789e36c1
Sergey Patrakov <patrakovsergo@gmail.com>
2023-03-10 19:10:35 +0300

    without script

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-const closeBtn = document.querySelector(".popup__button-close");
-const profileEditBtn = document.querySelector(".profile__button-edit");
-const popup = document.querySelector(".popup");
-closeBtn.addEventListener("click", () => popup.classList.remove("popup_opened"));
-profileEditBtn.addEventListener("click", () => popup.classList.add("popup_opened"));
